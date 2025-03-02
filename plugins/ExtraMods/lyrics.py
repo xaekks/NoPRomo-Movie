19d7b55f62cf3623bfdb3c79b2aafbc3de8fa780
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2025-03-02 16:29:48 +0530

    Update lyrics.py

--- plugins/ExtraMods/lyrics.py
+++ plugins/ExtraMods/lyrics.py
@@ -17,9 +17,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 📌 ᴜᴘᴅᴀᴛᴇs 📌", url = f"t.me/kissuxbots")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Cᴀɴ'ᴛ Fɪɴᴅ A Sᴏɴɢ Wɪᴛʜ `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/mkn_bots_updates")]]))
+             await message.reply_text(f"I Cᴀɴ'ᴛ Fɪɴᴅ A Sᴏɴɢ Wɪᴛʜ `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 📌 ᴜᴘᴅᴀᴛᴇs 📌", url = f"t.me/kissuxbots")]]))
 
 
 def search(song):
