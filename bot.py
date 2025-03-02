6925cd8cc8416bb50986a7dc8d58cbe0b2fe8948
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2025-03-02 20:09:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,106 +1,107 @@
+import os
+import math
 import logging
+import datetime
+import pytz
 import logging.config
+import asyncio
+from aiohttp import web, ClientSession
+from pyrogram import Client, types
+from database.users_chats_db import db
+from database.ia_filterdb import Media
+from typing import Union, Optional, AsyncGenerator
+from utils import temp, __repo__, __license__, __copyright__, __version__
+from info import API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, UPTIME, WEB_SUPPORT, LOG_MSG
 
 # Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.config.fileConfig("logging.conf")
+logging.getLogger(__name__).setLevel(logging.INFO)
+logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
-from utils import temp
-from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
-from Script import script 
-from datetime import date, datetime 
-import pytz
-from aiohttp import web
-from plugins import web_server
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            name=SESSION,
+            name="Professor-Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            workers=50,
-            plugins={"root": "plugins"},
-            sleep_threshold=5,
+            plugins=dict(root="plugins"),
         )
 
     async def start(self):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
+
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
-        temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
-        self.username = '@' + me.username
-        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
-        logging.info(LOG_STR)
-        logging.info(script.LOGO)
-        tz = pytz.timezone('Asia/Kolkata')
-        today = date.today()
-        now = datetime.now(tz)
-        time = now.strftime("%H:%M:%S %p")
-        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        self.id = me.id
+        self.name = me.first_name
+        self.mention = me.mention
+        self.username = me.username
+        self.log_channel = LOG_CHANNEL
+        self.uptime = UPTIME
+        curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
+        date = curr.strftime("%d %B, %Y")
+        time = curr.strftime("%I:%M:%S %p")
+        logging.info(
+            LOG_MSG.format(me.first_name, date, time, __repo__, __version__, __license__, __copyright__)
+        )
+
+        try:
+            await self.send_message(
+                LOG_CHANNEL,
+                text=LOG_MSG.format(me.first_name, date, time, __repo__, __version__, __license__, __copyright__),
+                disable_web_page_preview=True,
+            )
+        except Exception as e:
+            logging.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
+
+        if bool(WEB_SUPPORT) is True:
+            app = web.Application(client_max_size=30000000)
+            runner = web.AppRunner(app)
+            await runner.setup()
+            site = web.TCPSite(runner, "0.0.0.0", 8080)
+            await site.start()
+            logging.info("Web Response Is Running......🕸️")
 
     async def stop(self, *args):
         await super().stop()
-        logging.info("Bot stopped. Bye.")
+        logging.info("Bot Is Restarting ⟳...")
 
     async def iter_messages(
-        self,
-        chat_id: Union[int, str],
-        limit: int,
-        offset: int = 0,
+        self, chat_id: Union[int, str], limit: int, offset: int = 0
     ) -> Optional[AsyncGenerator["types.Message", None]]:
-        """Iterate through a chat sequentially.
-        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
-        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
-        single call.
-        Parameters:
-            chat_id (``int`` | ``str``):
-                Unique identifier (int) or username (str) of the target chat.
-                For your personal cloud (Saved Messages) you can simply use "me" or "self".
-                For a contact that exists in your Telegram address book you can use his phone number (str).
-                
-            limit (``int``):
-                Identifier of the last message to be returned.
-                
-            offset (``int``, *optional*):
-                Identifier of the first message to be returned.
-                Defaults to 0.
-        Returns:
-            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
-        Example:
-            .. code-block:: python
-                for message in app.iter_messages("pyrogram", 1, 15000):
-                    print(message.text)
-        """
         current = offset
         while True:
             new_diff = min(200, limit - current)
             if new_diff <= 0:
                 return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            messages = await self.get_messages(chat_id, list(range(current, current + new_diff + 1)))
             for message in messages:
                 yield message
                 current += 1
 
 
-app = Bot()
-app.run()
+URL = "https://scattered-catlaina-unknownr-5093cdff.koyeb.app/"  # Replace with your koyeb app link...
+
+
+async def ping():
+    async with ClientSession() as session:
+        while True:
+            try:
+                async with session.get(URL) as response:
+                    print(f"Pinged server, status: {response.status}")
+            except Exception as e:
+                print(f"{e}")
+            await asyncio.sleep(600)
+
+
+loop = asyncio.get_event_loop()
+loop.create_task(ping())
+
+Bot().run()
